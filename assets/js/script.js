b311a7711271c224a1565f567845e7fc75b3b749
Mavsic <muminova.m@hotmail.com>
2022-07-07 22:53:28 -0400

    final chnages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,40 @@
-// Displays current time in jumbotron
-var TimeElement =$("#currentTime");
-var now = moment();
-function updateTime() {
-    var now = momet().format("MM DD, YYYY");
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
+
+$(document).ready(function() {
+$(".saveBtn").on("click", function() {
+        
+var text = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id");
+
+localStorage.setItem(time, text);
+})
+ //get current number of hours   
+
+function timeTracker() {
+var timeNow = moment().hour();
+$(".time-block").each(function () {
+var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+       
+if (blockTime < timeNow) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+ }
+else if (blockTime === timeNow) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+}
+else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
 
 }
-TimeElement.text(now);
-setInterval(updateTime, 1000) 
-
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
-    }
+})
+}
 
     // Get item from local storage if any
     $("#hour8 .description").val(localStorage.getItem("hour8"));
